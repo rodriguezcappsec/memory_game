681e0645078092bee13288fdff8a55abbd4b58ce
Luis <luis.rodriguezcastro31@gmail.com>
2018-05-30 14:40:52 -0400

    new changes

--- js/main.js
+++ js/main.js
@@ -1,42 +1,40 @@
-let cards = [{
-        rank: 'queen',
-        suit: 'hearts',
-        cardImage: "images/queen-of-hearts.png",
-    },
-    {
-        rank: 'queen',
-        suit: 'diamonds',
-        cardImage: "images/queen-of-diamonds.png",
-    },
-    {
-        rank: 'king',
-        suit: 'hearts',
-        cardImage: "images/king-of-hearts.png",
-    },
-    {
-        rank: 'king',
-        suit: 'diamonds',
-        cardImage: "images/king-of-diamonds.png",
-    }
-];
+// let cards = [{
+//         rank: 'queen',
+//         suit: 'hearts',
+//         cardImage: "images/queen-of-hearts.png",
+//     },
+//     {
+//         rank: 'queen',
+//         suit: 'diamonds',
+//         cardImage: "images/queen-of-diamonds.png",
+//     },
+//     {
+//         rank: 'king',
+//         suit: 'hearts',
+//         cardImage: "images/king-of-hearts.png",
+//     },
+//     {
+//         rank: 'king',
+//         suit: 'diamonds',
+//         cardImage: "images/king-of-diamonds.png",
+//     }
+// ];
+// let cardsInPlay = [];
+
+
+let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log(`User flipped ${cardOne}`);
+console.log(`User flipped ${cardTwo}`);
 
-
-let checkForMatch = () => {
-    (cardsInPlay[0] === cardsInPlay[1]) ?
-    alert("You Found a Match"):
-        alert("Sorry, try again")
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("found a match");
+    } else {
+        alert("sorry try again");
+    }
 }
-
-let fliCard = (cardId) => {
-    checkForMatch();
-    cardsInPlay.push(cards[cardId].rank);
-    console.log(`User Flipped ${cards[cardId].rank}`);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-}
-
-
-
-fliCard(0);
-fliCard(2);
