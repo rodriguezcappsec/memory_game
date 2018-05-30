ae84f005185294c4d895c81bbcb5335aced235d5
Luis <luis.rodriguezcastro31@gmail.com>
2018-05-30 09:12:38 -0400

    array of cards

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-let queen = "queen";
-let king = "king";
-console.log(`Card Flipped ${queen}`);
-console.log(`Card Flipped ${king}`);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay.length === 2);
+cardsInPlay[0] === cardsInPlay[1] ? alert("is a Match") : alert("sorry try again");
